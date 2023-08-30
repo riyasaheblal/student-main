8f1f65f3e990ef8c9392dcd634ebb9da25873795
pradeep3015 <pradeep00970@gmail.com>
2023-08-30 17:50:16 +0530

    added the functions in daoimpl do fetch procedure dynamically

--- src/main/java/com/sl/student/secondary/util/CheckDb.java
+++ src/main/java/com/sl/student/secondary/util/CheckDb.java
@@ -1,29 +1,45 @@
 package com.sl.student.secondary.util;
 
 import com.sl.student.primary.config.DatasourceConfig;
+import jakarta.persistence.EntityManager;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 public class CheckDb {
 
 
     public static void main(String[] args) {
-        String s1 = "jdbc:oracle://localhost:3306/storedproducer";
 
-        if (s1.toLowerCase().contains("mysql")){
-            System.out.println("My-Sql");
-        }
-        else if (s1.toLowerCase().contains("sqlserver")){
-            System.out.println("Sql-Server");
-        }
-        else if (s1.toLowerCase().contains("oracle")){
-            System.out.println("Oracle");
-        }
-        else {
-            System.out.println("default Postgresql");
-        }
+        LocalContainerEntityManagerFactoryBean bds = entityManagerFactory();
+        //EntityManager entityManager = bds.getObject();
+        System.out.println(bds);
 
+        //DatasourceConfig dbConfig = new DatasourceConfig();
+        //dbConfig.connect("jdbc:mysql://192.168.101.134:3306/storedproducer","testpp","testpp");
+    }
 
-        DatasourceConfig dbConfig = new DatasourceConfig();
-        dbConfig.connect("jdbc:mysql://192.168.101.134:3306/storedproducer","testpp","testpp");
+    private static BasicDataSource getBasicDataSource(String classForName, String url, String userName, String password){
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://192.168.101.134:3306/storedproducer");
+        dataSource.setUsername("testpp");
+        dataSource.setPassword("testpp");
+
+        // Set connection pool settings
+        dataSource.setInitialSize(10);   // Initial number of connections
+        dataSource.setMaxTotal(50);
+        return dataSource;
+    }
+
+    public static LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(getBasicDataSource("","","",""));
+        entityManagerFactoryBean.setPackagesToScan("com.example.model"); // Replace with your package containing JPA entities
+        entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        // Additional JPA properties (if needed)
+        // entityManagerFactoryBean.setJpaProperties(jpaProperties);
+        return entityManagerFactoryBean;
     }
 
 }
